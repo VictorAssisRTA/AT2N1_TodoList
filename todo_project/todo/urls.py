53235c22bafd80c2484eff2c6c5ce390bc6c49b9
Victor_RTA <victorbr64g@gmail.com>
2025-06-24 16:21:56 -0300

    Part2

--- todo_project/todo/urls.py
+++ todo_project/todo/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('add/', views.task_create, name='task_create'),
-    path('edit/<int:pk>/', views.task_update, name='task_update'),
+    path('edit/<int:pk>/', views.task_edit, name='task_edit'),
     path('delete/<int:pk>/', views.task_delete, name='task_delete'),
 ]
+
