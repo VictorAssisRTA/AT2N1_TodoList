14eca0e5bd8775ae93e34269246a08269265746b
Victor_RTA <victorbr64g@gmail.com>
2025-06-24 18:06:56 -0300

    part4

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/victorassis/Documents/GitHub/AT2N1_TodoList/venv"
+set -gx VIRTUAL_ENV "/Users/victorassis/Documents/GitHub/AT2N1_TodoList/AT2N1_TODOLIST/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
